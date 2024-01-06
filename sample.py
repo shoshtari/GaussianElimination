f5f7287e496a4954e3b2b888976ff30f97688000
MohamadHossein Haghdadi <mmdhossein.haghdadi@gmail.com>
2024-01-06 20:18:42 +0330

    Small changes in sample.py file
    the main iteration is now on equations (rows) not columns

--- sample.py
+++ sample.py
@@ -1,42 +1,44 @@
-# a simple implementation of algorithm to have a guideline
-
-def solve(coefficients, products, n):
-    for i in range(n):
-        ind = i 
-        while coefficients[ind][i] == 0:
-            ind += 1
-            if ind == n:
-                raise ValueError("not solvable")
-            
-        coefficients[i], coefficients[ind] = coefficients[ind], coefficients[i]
-        products[i], products[ind] = products[ind], products[i]
-
-        products[i] = products[i] / coefficients[i][i]
-        coefficients[i] = [x / coefficients[i][i] for x in coefficients[i]]
+import random
+N = 10
+RandLowerBound = -100
+RandUpperBound = 100
 
 
+def solve(coefficients: list[list], products: list):
+    '''apply GaussianElimination to Solve N equations with coefficients and products matrices'''
 
+    order = [-1 for i in range(N)]
+    # order of variables in product matrix
 
-        for j in range(n):
-            if j == i:
+    for this_row in range(N):
+        index = 0
+        while coefficients[this_row][index] == 0:
+            index += 1
+        # find the first non-zero index in coefficients[this_row] equation
+
+        order[index] = this_row
+
+        products[this_row] /= coefficients[this_row][index]
+        coefficients[this_row] = [x / coefficients[this_row][index]
+                                  for x in coefficients[this_row]]
+
+        # make that index in other equations zero
+        for row in range(N):
+            if row == this_row:
                 continue
-            ratio = coefficients[j][i] # / coefficients[i][i]
-            products[j] -= ratio * products[i]
-            for k in range(i, n):
-                coefficients[j][k] -= ratio * coefficients[i][k]
+            coe = coefficients[row][index]
+            for column in range(N):
+                coefficients[row][column] -= coe * \
+                    coefficients[this_row][column]
+            products[row] -= coe * products[this_row]
 
-    return products
+    return [products[i] for i in order]
 
-n = 2
-# Gaussian Elimination
-coefficients = []
-for i in range(n):
-    coefficients.append(list(map(float, input().split())))
 
-products = []
-for i in range(n):
-    products.append(float(input()))
-
-print("ANS")
-for i in solve(coefficients, products, n):
-    print(i)
+if __name__ == "__main__":
+    coefficients = [[random.randint(RandLowerBound, RandUpperBound)
+                     for j in range(N)] for i in range(N)]
+    products = [random.randint(RandLowerBound, RandUpperBound)
+                for i in range(N)]
+    for i in solve(coefficients, products):
+        print(i)
