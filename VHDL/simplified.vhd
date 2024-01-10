9d626e59159c7bf1fc858e50768ea35b24789536
MohamadHossein Haghdadi <mmdhossein.haghdadi@gmail.com>
2024-01-10 10:21:04 +0330

    fix: fixed ans type

--- VHDL/simplified.vhd
+++ VHDL/simplified.vhd
@@ -12,11 +12,9 @@ ENTITY computeGaussian IS
         p1 : IN INTEGER;
         p2 : IN INTEGER;
 
-        ans1 : OUT INTEGER;
-        ans2 : OUT INTEGER;
+        ans1_vector : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+        ans2_vector : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
 
-        start_calculate : IN STD_LOGIC;
-        data_ready : INOUT STD_LOGIC
     );
 END ENTITY computeGaussian;
 
@@ -29,66 +27,68 @@ ARCHITECTURE rtl OF computeGaussian IS
             c : OUT INTEGER
         );
     END COMPONENT divider;
+    SIGNAL ans1 : INTEGER;
+    SIGNAL ans2 : INTEGER;
 BEGIN
 
-    calculate : PROCESS (start_calculate)
+
+    calculate : PROCESS (a1, a2, b1, b2, p1, p2)
 
         VARIABLE common_a1, common_a2, common_b1, common_b2, common_p1, common_p2 : INTEGER;
         VARIABLE ans1_var, ans2_var : INTEGER;
         VARIABLE step : INTEGER;
     BEGIN
-        IF rising_edge(start_calculate) THEN
-            common_a1 := a1 * a2;
-            common_b1 := b1 * a2;
-            common_p1 := p1 * a2;
+        common_a1 := a1 * a2;
+        common_b1 := b1 * a2;
+        common_p1 := p1 * a2;
 
-            common_a2 := a2 * a1;
-            common_b2 := b2 * a1;
-            common_p2 := p2 * a1;
+        common_a2 := a2 * a1;
+        common_b2 := b2 * a1;
+        common_p2 := p2 * a1;
 
-            common_a2 := common_a2 - common_a1;
-            common_b2 := common_b2 - common_b1;
-            common_p2 := common_p2 - common_p1;
+        common_a2 := common_a2 - common_a1;
+        common_b2 := common_b2 - common_b1;
+        common_p2 := common_p2 - common_p1;
 
-            -- til now:
-            -- ax + by = c 
-            -- dy = e;
+        -- til now:
+        -- ax + by = c 
+        -- dy = e;
 
-            ans2_var := 0;
+        ans2_var := 0;
 
-            IF (common_b2 < 0 AND common_p2 > 0) OR (common_p2 < 0 AND common_b2 > 0) THEN
-                step := - 1;
-            ELSE
-                step := 1;
-            END IF;
-
-            FOR count IN 0 TO 7 LOOP
-                IF common_b2 * ans2_var /= common_p2 THEN
-                    ans2_var := ans2_var + step;
-                END IF;
-            END LOOP;
-            common_p1 := common_p1 - (common_b1 * ans2_var);
-            ans1_var := 0;
-
-            IF (common_a1 < 0 AND common_p1 > 0) OR (common_p1 < 0 AND common_a1 > 0) THEN
-                step := - 1;
-            ELSE
-                step := 1;
-            END IF;
-
-            FOR count IN 0 TO 7 LOOP
-                IF common_a1 * ans1_var /= common_p1 THEN
-                    ans1_var := ans1_var + step;
-                END IF;
-            END LOOP;
-
-            ans1 <= ans1_var;
-            ans2 <= ans2_var;
-
-            data_ready <= '1';
+        IF (common_b2 < 0 AND common_p2 > 0) OR (common_p2 < 0 AND common_b2 > 0) THEN
+            step := - 1;
         ELSE
-            data_ready <= '0';
+            step := 1;
         END IF;
+
+        FOR count IN 0 TO 7 LOOP
+            IF common_b2 * ans2_var /= common_p2 THEN
+                ans2_var := ans2_var + step;
+            END IF;
+        END LOOP;
+        common_p1 := common_p1 - (common_b1 * ans2_var);
+        ans1_var := 0;
+
+        IF (common_a1 < 0 AND common_p1 > 0) OR (common_p1 < 0 AND common_a1 > 0) THEN
+            step := - 1;
+        ELSE
+            step := 1;
+        END IF;
+
+        FOR count IN 0 TO 7 LOOP
+            IF common_a1 * ans1_var /= common_p1 THEN
+                ans1_var := ans1_var + step;
+            END IF;
+        END LOOP;
+
+        ans1 <= ans1_var;
+        ans2 <= ans2_var;	
+		
+		
+    	ans1_vector <= STD_LOGIC_VECTOR(to_unsigned(ans1_var, 8));
+    	ans2_vector <= STD_LOGIC_VECTOR(to_unsigned(ans2_var, 8));
+
     END PROCESS;
 
 END ARCHITECTURE rtl;
--- VHDL/simplified_tb.vhd
+++ VHDL/simplified_tb.vhd
@@ -1,65 +1,57 @@
-LIBRARY ieee;
-USE ieee.NUMERIC_STD.ALL;
-USE ieee.std_logic_1164.ALL;
+library ieee;
+use ieee.NUMERIC_STD.all;
+use ieee.std_logic_1164.all;
 
--- Add your library and packages declaration here ...
+	-- Add your library and packages declaration here ...
 
-ENTITY computegaussian_tb IS
-END computegaussian_tb;
+entity computegaussian_tb is
+end computegaussian_tb;
 
-ARCHITECTURE TB_ARCHITECTURE OF computegaussian_tb IS
+architecture TB_ARCHITECTURE of computegaussian_tb is
 	-- Component declaration of the tested unit
-	COMPONENT computegaussian
-		PORT (
-			a1 : IN INTEGER;
-			b1 : IN INTEGER;
-			a2 : IN INTEGER;
-			b2 : IN INTEGER;
-			p1 : IN INTEGER;
-			p2 : IN INTEGER;
-			ans1 : OUT INTEGER;
-			ans2 : OUT INTEGER;
-			start_calculate : IN STD_LOGIC;
-			data_ready : INOUT STD_LOGIC);
-	END COMPONENT;
+	component computegaussian
+	port(
+		a1 : in INTEGER;
+		b1 : in INTEGER;
+		a2 : in INTEGER;
+		b2 : in INTEGER;
+		p1 : in INTEGER;
+		p2 : in INTEGER;
+		ans1_vector : out STD_LOGIC_VECTOR(7 downto 0);
+		ans2_vector : out STD_LOGIC_VECTOR(7 downto 0) );
+	end component;
 
 	-- Stimulus signals - signals mapped to the input and inout ports of tested entity
-	SIGNAL a1 : INTEGER;
-	SIGNAL b1 : INTEGER;
-	SIGNAL a2 : INTEGER;
-	SIGNAL b2 : INTEGER;
-	SIGNAL p1 : INTEGER;
-	SIGNAL p2 : INTEGER;
-	SIGNAL start_calculate : STD_LOGIC;
+	signal a1 : INTEGER := 0;
+	signal b1 : INTEGER := 0;
+	signal a2 : INTEGER := 0;
+	signal b2 : INTEGER := 0;
+	signal p1 : INTEGER := 0;
+	signal p2 : INTEGER := 0;
 	-- Observed signals - signals mapped to the output ports of tested entity
-	SIGNAL ans1 : INTEGER;
-	SIGNAL ans2 : INTEGER;
-	SIGNAL data_ready : STD_LOGIC;
+	signal ans1_vector : STD_LOGIC_VECTOR(7 downto 0);
+	signal ans2_vector : STD_LOGIC_VECTOR(7 downto 0);
 
 	-- Add your code here ...
 
-BEGIN
+begin
 
 	-- Unit Under Test port map
 	UUT : computegaussian
-	PORT MAP(
-		a1 => a1,
-		b1 => b1,
-		a2 => a2,
-		b2 => b2,
-		p1 => p1,
-		p2 => p2,
-		ans1 => ans1,
-		ans2 => ans2,
-		start_calculate => start_calculate,
-		data_ready => data_ready
-	);
+		port map (
+			a1 => a1,
+			b1 => b1,
+			a2 => a2,
+			b2 => b2,
+			p1 => p1,
+			p2 => p2,
+			ans1_vector => ans1_vector,
+			ans2_vector => ans2_vector
+		);
 
 	-- Add your stimulus here ...
 	PROCESS BEGIN
-		start_calculate <= '0';
-		WAIT FOR 100ns;
-
+		
 		a1 <= 2;
 		b1 <= 3;
 		p1 <= 8;
@@ -68,10 +60,6 @@ BEGIN
 		b2 <= 2;
 		p2 <= 7;
 
-		start_calculate <= '1';
-
-		WAIT UNTIL data_ready = '1';
-		start_calculate <= '0';
 		WAIT FOR 100ns;
 
 		a1 <= - 1;
@@ -82,10 +70,6 @@ BEGIN
 		b2 <= 2;
 		p2 <= 10;
 
-		start_calculate <= '1';
-
-		WAIT UNTIL data_ready = '1';
-		start_calculate <= '0';
 
 		WAIT FOR 100 ns;
 
@@ -97,32 +81,27 @@ BEGIN
 		b2 <= 1;
 		p2 <= 8;
 
-		start_calculate <= '1';
-
-		WAIT UNTIL data_ready = '1';
-		start_calculate <= '0';
 
 		WAIT FOR 100 ns;
 
 		a1 <= 3;
 		b1 <= 2;
-		p1 <= - 2;
+		p1 <= -2;
 
 		a2 <= 1;
 		b2 <= 1;
 		p2 <= 1;
 
-		start_calculate <= '1';
-
 		WAIT;
 	END PROCESS;
 
-END TB_ARCHITECTURE;
+end TB_ARCHITECTURE;
 
-CONFIGURATION TESTBENCH_FOR_computegaussian OF computegaussian_tb IS
-	FOR TB_ARCHITECTURE
-		FOR UUT : computegaussian
-			USE ENTITY work.computegaussian(rtl);
-		END FOR;
-	END FOR;
-END TESTBENCH_FOR_computegaussian;
+configuration TESTBENCH_FOR_computegaussian of computegaussian_tb is
+	for TB_ARCHITECTURE
+		for UUT : computegaussian
+			use entity work.computegaussian(rtl);
+		end for;
+	end for;
+end TESTBENCH_FOR_computegaussian;
+
